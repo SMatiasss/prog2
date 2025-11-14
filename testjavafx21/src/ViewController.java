1073d34aab09699ac8e66388ff5e33fcf1ff0005
SMatiasss <ssoriamatiass@gmail.com>
2025-11-13 21:00:48 -0300

    primer commit

---
+++ testjavafx21/src/ViewController.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ViewController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
---
+++ testjavafx21/src/testjavafx21/Testjavafx21.java
@@ -0,0 +1,16 @@
+package testjavafx21;
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Testjavafx21 extends Application{
+    
+    public void start(Stage stage) throws Exception {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
